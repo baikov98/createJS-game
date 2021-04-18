b8559bd26e9da879a404cfaededefa15abe7c148
Vilmir Baikov <noname777228@rambler.ru>
2021-04-18 22:23:03 +0500

    jump, gameover

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var stage, loader;
+var stage, loader, flappy;
 
 function init() {
     stage = new createjs.StageGL("gameCanvas");
@@ -28,29 +28,51 @@ function init() {
     loader.loadManifest(manifest, true, "./img/")
   }
 
-  function handleComplete() {
-    createClouds();
-  }
+function handleComplete() {
+  createClouds();
+  createFlappy()
+  stage.on("stagemousedown", jumpFlappy);
+}
 
-  function createClouds() {
-    var clouds = [];
-    for (var i = 0; i < 3; i++) {
-      clouds.push(new createjs.Bitmap(loader.getResult("cloud")));
-    }
-    clouds[0].x = 40;
-    clouds[0].y = 20;
-    clouds[1].x = 140;
-    clouds[1].y = 70;
-    clouds[2].x = 100;
-    clouds[2].y = 130;
-    for (var i = 0; i < 3; i++) {
-        var directionMultiplier = i % 2 == 0 ? -1 : 1;
-        createjs.Tween.get(clouds[i], { loop: true})
-        .to({ x: clouds[i].x - (200 * directionMultiplier)}, 3000, createjs.Ease.getPowInOut(2))
-        .to({ x: clouds[i].x }, 3000, createjs.Ease.getPowInOut(2));
-        stage.addChild(clouds[i]);
-      }
-      stage.update();
+function createClouds() {
+  var clouds = [];
+  for (var i = 0; i < 3; i++) {
+    clouds.push(new createjs.Bitmap(loader.getResult("cloud")));
   }
+  clouds[0].x = 40;
+  clouds[0].y = 20;
+  clouds[1].x = 140;
+  clouds[1].y = 70;
+  clouds[2].x = 100;
+  clouds[2].y = 130;
+  for (var i = 0; i < 3; i++) {
+      var directionMultiplier = i % 2 == 0 ? -1 : 1;
+      createjs.Tween.get(clouds[i], { loop: true})
+      .to({ x: clouds[i].x - (200 * directionMultiplier)}, 3000, createjs.Ease.getPowInOut(2))
+      .to({ x: clouds[i].x }, 3000, createjs.Ease.getPowInOut(2));
+      stage.addChild(clouds[i]);
+    }
+    stage.update();
+}
+
+function createFlappy() {
+  flappy = new createjs.Bitmap(loader.getResult("flappy"));
+  flappy.regX = flappy.image.width / 2;
+  flappy.regY = flappy.image.height / 2;
+  flappy.x = stage.canvas.width / 2;
+  flappy.y = stage.canvas.height / 2;
+  stage.addChild(flappy);
+}
+
+function jumpFlappy() {
+  createjs.Tween.get(flappy, { override: true })
+  .to({ y: flappy.y - 60, rotation: -10 }, 500, createjs.Ease.getPowOut(2))
+  .to({ y: stage.canvas.height + (flappy.image.width / 2), rotation: 30 }, 1500, createjs.Ease.getPowIn(2))
+  .call(gameOver);
+}
+ 
+function gameOver() {
+  console.log("Game over!");
+}
 
 
