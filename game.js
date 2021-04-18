84e95173e8e2023bae101aaae3eef1e8a8495289
Vilmir Baikov <noname777228@rambler.ru>
2021-04-18 22:56:51 +0500

    create pipes

--- game.js
+++ game.js
@@ -31,6 +31,7 @@ function init() {
 function handleComplete() {
   createClouds();
   createFlappy()
+  createPipes()
   stage.on("stagemousedown", jumpFlappy);
 }
 
@@ -64,13 +65,42 @@ function createFlappy() {
   stage.addChild(flappy);
 }
 
+
 function jumpFlappy() {
   createjs.Tween.get(flappy, { override: true })
   .to({ y: flappy.y - 60, rotation: -10 }, 500, createjs.Ease.getPowOut(2))
   .to({ y: stage.canvas.height + (flappy.image.width / 2), rotation: 30 }, 1500, createjs.Ease.getPowIn(2))
   .call(gameOver);
 }
+
+function createPipes() {
+  var topPipe, bottomPipe;
+  var position = Math.floor(Math.random() * 280 + 100);
  
+  topPipe = new createjs.Bitmap(loader.getResult("pipe"));
+  topPipe.y = position - 75;
+  topPipe.x = stage.canvas.width + (topPipe.image.width / 2);
+  topPipe.rotation = 180;
+  topPipe.name = "pipe";
+ 
+  bottomPipe = new createjs.Bitmap(loader.getResult("pipe"));
+  bottomPipe.y = position + 75;
+  bottomPipe.x = stage.canvas.width + (bottomPipe.image.width / 2);
+  bottomPipe.skewY = 180;
+  bottomPipe.name = "pipe";
+ 
+  topPipe.regX = bottomPipe.regX = topPipe.image.width / 2;
+ 
+  createjs.Tween.get(topPipe).to({ x: 0 - topPipe.image.width }, 10000).call(function() { removePipe(topPipe); });
+  createjs.Tween.get(bottomPipe).to( { x: 0 - bottomPipe.image.width }, 10000).call(function() { removePipe(bottomPipe); });
+ 
+  stage.addChild(bottomPipe, topPipe);
+}
+
+function removePipe(pipe) {
+  stage.removeChild(pipe);
+}
+
 function gameOver() {
   console.log("Game over!");
 }
